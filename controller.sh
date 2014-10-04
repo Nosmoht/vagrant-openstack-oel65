41d7016c7ae2d54305a5a8227831f366f8cdc5d4
Nosmoht <ntbc@gmx.net>
2014-10-04 18:22:21 +0200

    Add second compute node

--- controller.sh
+++ controller.sh
@@ -1,3 +1,3 @@
 yum install -y openstack-packstack
-sudo packstack --install-hosts=192.168.0.200,192.168.0.201 --use-epel=n --neutron-ovs-tenant-network-type=vlan --neutron-ovs-vlan-range=default:1000:2000 --neutron-ovs-bridge-mappings=default:br-eth2 --neutron-ovs-bridge-interfaces=br-eth2:eth2
+sudo packstack --install-hosts=192.168.0.200,192.168.0.201,192.168.0.202 --use-epel=n --neutron-ovs-tenant-network-type=vlan --neutron-ovs-vlan-range=default:1000:2000 --neutron-ovs-bridge-mappings=default:br-eth2 --neutron-ovs-bridge-interfaces=br-eth2:eth2
 
